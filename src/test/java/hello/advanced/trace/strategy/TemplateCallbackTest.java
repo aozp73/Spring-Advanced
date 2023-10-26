acde964f6ec318e554d4172f9933842cd36478a7
aozp73 <aozp73@naver.com>
2023-10-26 22:03:56 +0900

    템플릿 콜백 패턴 - 예제

---
+++ src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.template.CallBack;
+import hello.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+    /**
+     * 템플릿 콜백 패턴 - 익명 내부 클래스
+     */
+    @Test
+    void CallbackV1() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(new CallBack() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+
+        template.execute(new CallBack() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 템플릿 콜백 패턴 - 람다
+     */
+    @Test
+    void CallbackV2() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(() -> log.info("비지니스 로직1 실행"));
+        template.execute(() -> log.info("비지니스 로직2 실행"));
+    }
+
+}
